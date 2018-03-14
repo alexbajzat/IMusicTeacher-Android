9a1c642c05cb8bad2eafc4438d0087a1200437e7
alex-bajzat <alex.bajzat@gmail.com>
2018-03-14 14:33:38 +0200

    added a mock server in testing.

--- app/src/main/java/com/bjz/imusicteacher/service/PredictionModelService.java
+++ app/src/main/java/com/bjz/imusicteacher/service/PredictionModelService.java
@@ -1,9 +1,14 @@
 package com.bjz.imusicteacher.service;
 
+import com.bjz.imusicteacher.model.PredictionModel;
+
+import retrofit2.http.GET;
+
 /**
  * Created by bjz on 3/14/2018.
  */
 
 public interface PredictionModelService {
-
+    @GET(value = "/network/model")
+    PredictionModel getModel();
 }

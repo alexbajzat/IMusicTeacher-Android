334b54c8dd3149f268c40ff50e9d5589940a958e
alex-bajzat <alex.bajzat@gmail.com>
2018-03-20 13:12:58 +0200

    Added activity with a camera preview

--- app/src/main/java/com/bjz/imusicteacher/MainActivity.java
+++ app/src/main/java/com/bjz/imusicteacher/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bjz.imusicteacher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bjz.imusicteacher.activity.ProcessingActivity;
 import com.bjz.imusicteacher.model.descriptor.ModelDescriptor;
 import com.bjz.imusicteacher.model.network.NetworkModel;
 import com.bjz.imusicteacher.model.network.NetworkModelBuilder;
@@ -20,25 +22,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.56.1:3000")
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
-        PredictionModelService service = retrofit.create(PredictionModelService.class);
 
-        service.getModel().enqueue(new Callback<ModelDescriptor>() {
-            @Override
-            public void onResponse(Call<ModelDescriptor> call, Response<ModelDescriptor> response) {
-                ModelDescriptor body = response.body();
-                NetworkModel model = NetworkModel.builder()
-                        .build(body);
-            }
+        Intent intent = new Intent(this, ProcessingActivity.class);
+        startActivity(intent);
 
-            @Override
-            public void onFailure(Call<ModelDescriptor> call, Throwable t) {
-                throw new RuntimeException("CALL FAILURE" + t.getMessage());
-            }
-        });
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("http://192.168.56.1:3000")
+//                .addConverterFactory(JacksonConverterFactory.create())
+//                .build();
+//        PredictionModelService service = retrofit.create(PredictionModelService.class);
+//
+//        service.getModel().enqueue(new Callback<ModelDescriptor>() {
+//            @Override
+//            public void onResponse(Call<ModelDescriptor> call, Response<ModelDescriptor> response) {
+//                ModelDescriptor body = response.body();
+//                NetworkModel model = NetworkModel.builder()
+//                        .build(body);
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ModelDescriptor> call, Throwable t) {
+//                throw new RuntimeException("CALL FAILURE" + t.getMessage());
+//            }
+//        });
 
     }
 }
---
+++ app/src/main/java/com/bjz/imusicteacher/activity/ProcessingActivity.java
@@ -0,0 +1,204 @@
+package com.bjz.imusicteacher.activity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Toast;
+
+
+import com.bjz.imusicteacher.R;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProcessingActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    @BindView(R.id.previewTextureView)
+    TextureView previewTextureView;
+    private CameraDevice mCameraDevice;
+    private String mCameraId;
+    private Size imageDimension;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private CameraCaptureSession mCameraCaptureSession;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
+
+    CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice) {
+            mCameraDevice = cameraDevice;
+            startCameraPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+            mCameraDevice.close();
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int i) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    };
+    TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+            initCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_processing);
+        ButterKnife.bind(this);
+        initPreviewTextureView();
+
+    }
+
+    private void initPreviewTextureView() {
+        previewTextureView.setSurfaceTextureListener(surfaceTextureListener);
+    }
+
+    private void initCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        assert cameraManager != null;
+
+        try {
+            mCameraId = cameraManager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(mCameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                return;
+            }
+            cameraManager.openCamera(mCameraId, cameraStateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startCameraPreview() {
+        try {
+            SurfaceTexture texture = previewTextureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            Surface surface = new Surface(texture);
+            mCaptureRequestBuilder.addTarget(surface);
+            mCameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    if (mCameraDevice == null || !previewTextureView.isAvailable() || imageDimension == null) {
+                        Toast.makeText(getApplicationContext(), "Preview not available. Configuration failed", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    mCameraCaptureSession = cameraCaptureSession;
+                    updatePreview();
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                    Toast.makeText(getApplicationContext(), "Configuration change", Toast.LENGTH_SHORT).show();
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void updatePreview() {
+        if (mCameraDevice == null) {
+            Toast.makeText(this, "Camera error", Toast.LENGTH_LONG).show();
+        }
+        mCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
+        try {
+            mCameraCaptureSession.setRepeatingRequest(mCaptureRequestBuilder.build(), null, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startBackgroundThread();
+        if (previewTextureView.isAvailable()) {
+            initCamera();
+        } else {
+            previewTextureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        stopBackgroundThread();
+        super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "You can`t use camera without permission", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

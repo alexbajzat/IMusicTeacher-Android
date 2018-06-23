fe6ac3c3cba684dbd94e20ab042fa0222f5f47aa
alex-bajzat <alex.bajzat@gmail.com>
2018-06-24 00:33:58 +0300

    Version problem solved;

--- app/src/main/java/com/bjz/imusicteacher/MainActivity.java
+++ app/src/main/java/com/bjz/imusicteacher/MainActivity.java
@@ -1,20 +1,9 @@
 package com.bjz.imusicteacher;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import com.bjz.imusicteacher.activity.ProcessingActivity;
-import com.bjz.imusicteacher.model.descriptor.ModelDescriptor;
-import com.bjz.imusicteacher.model.network.NetworkModel;
-import com.bjz.imusicteacher.model.network.NetworkModelBuilder;
-import com.bjz.imusicteacher.service.PredictionModelService;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +14,5 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, ProcessingActivity.class);
         startActivity(intent);
-
-
-
     }
 }
